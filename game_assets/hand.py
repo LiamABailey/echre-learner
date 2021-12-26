d6a80b72e1a9e887f5cc755cd5f8727c332fc10a
LiamABailey <bailey.liam102@gmail.com>
2021-12-26 00:04:12 -0500

    Additional specification of hand

--- game_assets/hand.py
+++ game_assets/hand.py
@@ -1,16 +1,24 @@
 from typing import Union
 
+from euchre import NUM_TRICKS
+from trick import Trick
+
 class Hand:
 
-    def __init__(self, trump: Union[int,str]) -> None:
+    def __init__(self, bidder: int, trump: Union[int,str]) -> None:
         """
         Hand constructor (encapsulating 5 tricks). Requires specification of
         the trump suit
 
         Parameters
         ----------
+            bidder : int
+                the seat index of the bidder (player who chose trump)
+
             trump : int or str
                 One of the suits in euchre.SUITS
+
+
 
         Returns
         -------
@@ -18,3 +26,38 @@ class Hand:
         """
         self.tricks = []
         self.trump = trump
+        self.bidder = bidder
+
+
+    def add_trick(self, played_trick: Trick) -> None:
+        """
+        add a played trick to the trick storer.
+
+        Parameters
+        ----------
+            played_trick : Trick
+                The trick to track
+
+        Returns
+        -------
+            None
+
+        """
+            if len(self.tricks) >= NUM_TRICKS:
+                raise ValueError("Attempting to add unexpected trick!")
+            self.tricks.append(played_trick)
+
+    def score_hand(self) -> Tuple(bool, int):
+        """
+        Score a hand of (5) played tricks, and return the winning team + score
+
+        Parameters
+        ----------
+            None
+
+        Returns
+        -------
+            bool : True if the 0th team won, 1 otherwise
+            int : the number of points scored
+        """
+        
