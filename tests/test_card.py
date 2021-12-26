57303c5db28ac4b362955a36922e838c0aecf6f5
LiamABailey <bailey.liam102@gmail.com>
2021-12-26 15:18:07 -0500

    Additional tests of _is_left_bar

--- tests/test_card.py
+++ tests/test_card.py
@@ -18,14 +18,18 @@ class TestIsLeftBar(unittest.TestCase):
         """
         Test a basic case where the card is not the left bar
         """
-        pass
+        trump = euchre.DIAMOND
+        c_9 = card.Card(euchre.CLUB, euchre.NINE)
+        self.assertFalse(c_9._is_left_bar(trump))
 
 
     def test_is_left_bar_right(self):
         """
         Test a basic case where the card is the right bar
         """
-        pass
+        trump = euchre.SPADE
+        r_j = card.Card(euchre.SPADE, euchre.JACK)
+        self.assertFalse(r_j._is_left_bar(trump))
 
 
     def test_is_left_bar_offsuit_jack(self):
@@ -33,4 +37,6 @@ class TestIsLeftBar(unittest.TestCase):
         Test a basic case where the card is a jack, but neither the
         left or right
         """
-        pass
+        trump = euchre.DIAMOND
+        c_9 = card.Card(euchre.CLUB, euchre.JACK)
+        self.assertFalse(c_9._is_left_bar(trump))
