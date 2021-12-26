8059afb721e57c272ae32f754738f2e6ff2dced6
LiamABailey <bailey.liam102@gmail.com>
2021-12-26 15:22:48 -0500

    Tests of card.is_trump

--- tests/test_card.py
+++ tests/test_card.py
@@ -19,7 +19,8 @@ class TestIsLeftBar(unittest.TestCase):
         Test a basic case where the card is not the left bar
         """
         trump = euchre.DIAMOND
-        c_9 = card.Card(euchre.CLUB, euchre.NINE)
+        other_suit = euchre.CLUB
+        c_9 = card.Card(other_suit, euchre.NINE)
         self.assertFalse(c_9._is_left_bar(trump))
 
 
@@ -28,7 +29,7 @@ class TestIsLeftBar(unittest.TestCase):
         Test a basic case where the card is the right bar
         """
         trump = euchre.SPADE
-        r_j = card.Card(euchre.SPADE, euchre.JACK)
+        r_j = card.Card(trump, euchre.JACK)
         self.assertFalse(r_j._is_left_bar(trump))
 
 
@@ -38,5 +39,45 @@ class TestIsLeftBar(unittest.TestCase):
         left or right
         """
         trump = euchre.DIAMOND
-        c_9 = card.Card(euchre.CLUB, euchre.JACK)
+        other_suit = euchre.CLUB
+        c_9 = card.Card(other_suit, euchre.JACK)
         self.assertFalse(c_9._is_left_bar(trump))
+
+
+class TestIsTrump(unittest.TestCase):
+
+    def test_is_trump_true(self):
+        """
+        Test where card is in the trump suit
+        """
+        trump = euchre.HEART
+        h_10 = card.Card(trump, euchre.TEN)
+        self.assertTrue(h_10.is_trump(trump))
+
+
+    def test_is_trump_false(self):
+        """
+        Test where card is not in the trump suit
+        """
+        trump = euchre.HEART
+        other_suit = euchre.CLUB
+        c_a = card.Card(other_suit, euchre.ACE)
+        self.assertFalse(c_a.is_trump(trump))
+
+
+    def test_is_trump_left_bar(self):
+        """
+        Confirm that the left bar is identified as trump
+        """
+        trump = euchre.SPADE
+        l_j = card.Card(euchre.LEFT_SUIT[trump], euchre.JACK)
+        self.assertTrue(l_j.is_trump(trump))
+
+
+    def test_is_trump_right_bar(self):
+        """
+        Confirm that the right bar is identified as trump
+        """
+        trump = euchre.SPADE
+        r_j = card.Card(trump, euchre.JACK)
+        self.assertTrue(r_j.is_trump(trump))
