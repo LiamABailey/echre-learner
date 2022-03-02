1976f2fae6c67e4f18460bb9e881ab5fa3c1165b
LiamABailey <bailey.liam102@gmail.com>
2022-03-01 21:02:01 -0500

    Removed unsupported str typing from card face, suit

--- game_assets/card.py
+++ game_assets/card.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Union
 
 from .euchre import CARD_FACES, SUIT_DESCRIPTOR, FACE_DESCRIPTOR, JACK, LEFT_SUIT
 
@@ -8,22 +7,22 @@ class Card:
     """
     Class for storing card information
     """
-    suit: Union[str, int]
-    face: Union[str, int]
+    suit: int
+    face: int
 
 
     def __str__(self):
         return (f"{FACE_DESCRIPTOR[self.face].capitalize()} of "
                 f"{SUIT_DESCRIPTOR[self.suit].capitalize()}s")
 
-    def _is_left_bar(self, trump: Union[str, int]) -> bool:
+    def _is_left_bar(self, trump: int) -> bool:
         """
         Evaluate if the card is specifically the left bar of a given
         trump suit
 
         Parameters
         ----------
-            trump : Union[str, int]
+            trump : int
                 The trump suit, from euchre.SUITS
 
         Returns
@@ -33,13 +32,13 @@ class Card:
         if self.face == JACK and self.suit == LEFT_SUIT[trump]:
             return True
 
-    def is_trump(self, trump: Union[str, int]) -> bool:
+    def is_trump(self, trump: int) -> bool:
         """
         Evaluate if the card is in the trump suit
 
         Parameters
         ----------
-            trump : Union[str, int]
+            trump : int
                 The trump suit, from euchre.SUITS
 
         Returns
@@ -50,7 +49,7 @@ class Card:
             return True
         return False
 
-    def lt_card(self, other: 'Card', trump: Union[str, int], lead: Union[str, int]) -> bool:
+    def lt_card(self, other: 'Card', trump: int, lead: int) -> bool:
         """
         Check if this card is of lower value than another card, given
         the trump suit and leading suit
@@ -60,10 +59,10 @@ class Card:
             other : Card
                 The card compared
 
-            trump : Union[str, int]
+            trump : int
                 The trump suit, from euchre.SUITS
 
-            trump : Union[str, int]
+            trump : int
                 The  suit that started the trick, from euchre.SUITS
 
         Returns
